5306033a9bc4adedd672301c8a4c39f0770d2bbe
yj718603 <105530075+yj718603@users.noreply.github.com>
2025-03-27 17:35:43 +0800

    Update astro.config.mjs
    // @ts-check
    import { defineConfig } from 'astro/config';
    
    // https://astro.build/config
    export default defineConfig({});

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
-export default defineConfig({
-  adapter: netlify()
-});
+export default defineConfig({});
