dbfbfd49b5df404917d25b20f1e3c336cd8ee2d0
yj718603 <105530075+yj718603@users.noreply.github.com>
2025-03-28 15:52:27 +0800

    Update astro.config.mjs
    import { defineConfig } from 'astro/config';
    import node from '@astrojs/node';
    
    export default defineConfig({
      output: 'server',   // 启用 SSR
      adapter: node({ mode: 'standalone' })  // Node 适配器，独立模式
    });

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-//
-// https://astro.build/config
+import node from '@astrojs/node';
+
 export default defineConfig({
-  output: 'server'
+  output: 'server',   // 启用 SSR
+  adapter: node({ mode: 'standalone' })  // Node 适配器，独立模式
 });
-//import { defineConfig } from 'astro/config';
-//import node from '@astrojs/node';
-//export default defineConfig({
-  //output: 'server',             // 重要：启用 SSR 模式
-  //adapter: node({ mode: 'standalone' })  // 'standalone' 模式更适合 Azure Web App
-//});
-//import { defineConfig } from 'astro/config';
-//import netlify from '@astrojs/netlify'; // <-- 确保这一行存
-//export default defineConfig({
-//  output: 'server',   // Astro SSR 必须加上 `output: 'server'`
-//  adapter: netlify() // 使用 Netlify 适配器
-//});
