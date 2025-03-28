6d2f9202855e890c8a9e97d55df6cb3e456e657c
yj718603 <105530075+yj718603@users.noreply.github.com>
2025-03-28 11:31:31 +0800

    Update astro.config.mjs
    output: 'server'

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@
 import { defineConfig } from 'astro/config';
 //
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server'
+});
 //import { defineConfig } from 'astro/config';
 //import node from '@astrojs/node';
 //export default defineConfig({
