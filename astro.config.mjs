095f9a11232a1b3bc98b7c21e78041b196253b7f
yj718603 <105530075+yj718603@users.noreply.github.com>
2025-03-28 17:06:55 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
+  server: {
+    port: 8080,  // 设置开发服务器端口为 8080
+  },
   output: 'server',   // 启用 SSR
   adapter: node({ mode: 'standalone' })  // Node 适配器，独立模式
 });
