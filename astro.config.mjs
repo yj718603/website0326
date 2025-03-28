14b30f7af7abb422986ddb33e29d48f42f817930
yj718603 <105530075+yj718603@users.noreply.github.com>
2025-03-28 09:20:32 +0800

    Update astro.config.mjs
    import { defineConfig } from 'astro/config';
    import node from '@astrojs/node';
    
    export default defineConfig({
      output: 'server',             // 重要：启用 SSR 模式
      adapter: node({ mode: 'standalone' })  // 'standalone' 模式更适合 Azure Web App
    });

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+//import { defineConfig } from 'astro/config';
+//
 // https://astro.build/config
-export default defineConfig({});
+//export default defineConfig({});
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+export default defineConfig({
+  output: 'server',             // 重要：启用 SSR 模式
+  adapter: node({ mode: 'standalone' })  // 'standalone' 模式更适合 Azure Web App
+});
